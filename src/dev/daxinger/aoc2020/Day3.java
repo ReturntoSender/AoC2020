cfa1eeb3a0cb893b37b346e2ae61db256bb4fbbd
daXinger <TerraXF@gmail.com>
2020-12-09 19:57:19 +0100

    Day3 fertig

--- src/dev/daxinger/aoc2020/Day3.java
+++ src/dev/daxinger/aoc2020/Day3.java
@@ -6,7 +6,7 @@ public class Day3 extends AOCPuzzle
 {
     public Day3()
     {
-        super("test");
+        super("3");
     }
 
     @Override
@@ -15,9 +15,54 @@ public class Day3 extends AOCPuzzle
         puzzleB(input);
     }
 
-    void puzzleB(List<String> input) {
+    void puzzleA(List<String> input) {
+
+        int x = 0;
+        int y = 0;
+        int deltaX = 3;
+        int deltaY = 1;
+        int treeCount = 0;
+
+        for (int i = 0; i < input.size()-1; i += deltaY ) {
+            x += deltaX;
+            //System.out.println("X: " + x);
+            y += deltaY;
+            //System.out.println("Y: " + y);
+
+            if (input.get(y).charAt(x%input.get(y).length()) == '#') {
+                treeCount++;
+            }
+            //System.out.println(input.get(y).charAt(x%input.get(y).length()));
+        }
+        System.out.println("You hit " + treeCount + " trees in puzzle 1");
+
     }
 
-    void puzzleA(List<String> input) {
+    void puzzleB(List<String> input) {
+
+        int[] deltaX = {1, 3, 5, 7, 1};
+        int[] deltaY = {1, 1, 1, 1, 2};
+        int treeCount;
+        long prud = 1;
+
+        for (int j = 0; j < deltaX.length; j++) {
+            treeCount = 0;
+            int x = 0;
+            int y = 0;
+            for (int i = 0; i < input.size()-1; i += deltaY[j] ) {
+                x += deltaX[j];
+                //System.out.println("X: " + x);
+                y += deltaY[j];
+                //System.out.println("Y: " + y);
+
+                if (input.get(y).charAt(x%input.get(y).length()) == '#') {
+                    treeCount++;
+                }
+
+                //System.out.println(input.get(y).charAt(x%input.get(y).length()));
+            }
+            prud *= treeCount;
+        }
+        System.out.println("You hit " + prud + " trees in puzzle 2");
     }
 }
