4885bbcf1d3666dc0a57f48f7b4a899e006e38ef
daXinger <TerraXF@gmail.com>
2020-12-09 17:22:55 +0100

    Day2 fertig

--- src/dev/daxinger/aoc2020/Day2.java
+++ src/dev/daxinger/aoc2020/Day2.java
@@ -6,12 +6,13 @@ public class Day2 extends AOCPuzzle
 {
     public Day2()
     {
-        super("test");
+        super("2");
     }
 
     @Override
     void solve(List<String> input) {
         puzzleA(input);
+        puzzleB(input);
     }
 
     public void puzzleA (List<String> input) {
@@ -33,19 +34,40 @@ public class Day2 extends AOCPuzzle
 
             int counter = 0;
             for (int i = 0; i < password.length(); i++) {
-                System.out.println(password.charAt(i) + " " + key);
+                //System.out.println(password.charAt(i) + " " + key);
                 if (password.charAt(i) == key) {
                     counter++;
                 }
                 //System.out.println(counter);
             }
             //System.out.println("Counter steht bei " + counter);
-            if (counter < max && counter > min) {
+            if (counter <= max && counter >= min) {
                 valids++;
-                System.out.println(valids);
+                //System.out.println(valids);
 
             }
         }
-        System.out.println("Anzahl der Vailds:" + valids);
+        System.out.println("Anzahl der Vailds Aufgabe 1: " + valids);
+    }
+
+    public void puzzleB (List<String> input) {
+        int valids = 0;
+        for (String line : input) {
+            String[] cut = line.split(" ");
+
+            String[] minmax = cut[0].split("-");
+            int min = Integer.parseInt(minmax[0]);
+            int max = Integer.parseInt(minmax[1]);
+
+            char key = cut[1].charAt(0);
+
+            String password = cut[2];
+
+            if (password.charAt(min-1) == key ^ password.charAt(max-1) == key) {
+                valids++;
+            }
+
+        }
+        System.out.println("Anzahl der Vailds Aufgabe 2: " + valids);
     }
 }
