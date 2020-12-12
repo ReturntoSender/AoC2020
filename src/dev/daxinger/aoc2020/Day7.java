54b803aa7d767a378ec95141ef45990f92a2838e
daXinger <TerraXF@gmail.com>
2020-12-12 09:20:50 +0100

    Day 7 mit fehler

--- src/dev/daxinger/aoc2020/Day7.java
+++ src/dev/daxinger/aoc2020/Day7.java
@@ -1,19 +1,12 @@
 package dev.daxinger.aoc2020;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-public class Day7 extends AOCPuzzle
-{
-    public Day7()
-    {
+public class Day7 extends AOCPuzzle {
+    public Day7() {
         super("test");
     }
 
-    public static List<String> containments = new ArrayList<String>();
-    public static Map<String, Bags> bag = new HashMap<>();
 
     @Override
     void solve(List<String> input) {
@@ -22,74 +15,89 @@ public class Day7 extends AOCPuzzle
     }
 
     void puzzleA(List<String> input) {
-        splitInput(input);
+        String color = "shinygold";
+        Map<String, List<String>> bags = splitInput(input);
+        Set<String> soManyGold = searchBag(color, bags);
+        System.out.println("In " + soManyGold.size() + " bags is gold");
     }
 
     void puzzleB(List<String> input) {
 
     }
 
-    void splitInput(List<String> input) {
+    Map splitInput(List<String> input) {
+        List<String> containment = new ArrayList<String>();
+        Map<String, List<String>> bag = new HashMap<>();
+        int max = 0;
+
         for (String s : input) {
-
             String[] cut = s.split(" ");
-            if (cut.length == 12) {
-                containments.add(cut[4]);
-                containments.add(cut[5] + cut[6]);
-                containments.add(cut[8]);
-                containments.add(cut[9] + cut[10]);
-                bag.put(cut[0]+cut[1], new Bags(cut[0]+cut[1], containments));
-                containments.clear();
+            if (cut.length > max) max = cut.length;
+            switch(cut.length) {
+                case 7:
+                    containment.add("");
+                    break;
+                case 8:
+                    List<String> newlist = Arrays.asList(cut[4], cut[5] + cut[6]);
+                    containment.addAll(newlist);
+                    break;
+                case 12:
+                    containment.addAll(Arrays.asList(cut[8], cut[9] + cut[10]));
+                    break;
+                case 16:
+                    containment.addAll(Arrays.asList(cut[12], cut[13] + cut[14]));
+                    break;
+                case 20:
+                    containment.addAll(Arrays.asList(cut[16], cut[17] + cut[18]));
+                    break;
             }
-            else if (cut.length == 8) {
-                containments.add(cut[4]);
-                containments.add(cut[5] + cut[6]);
-                bag.put(cut[0]+cut[1], new Bags(cut[0]+cut[1], containments));
-                containments.clear();
-            }
-            else if (cut.length == 7) {
-                bag.put(cut[0]+cut[1], new Bags(cut[0]+cut[1]));
-
-            }
-            else {
-                System.out.println("Fehler: unbekannte Stringlänge");
+            bag.put(cut[0] + cut[1], containment);
+            for (Map.Entry<String, List<String>> en : bag.entrySet()) {
+                System.out.println(en.getKey() + " " + en.getValue());
             }
         }
-        System.out.println(bag.get("lightred").getInsideBags().get(0));
+        return bag;
     }
 
-    public class Bags {
+    Set<String> searchBag(String color, Map<String, List<String>> bag) {
+        Set<String> containsGold = new HashSet<>();
+        int counter = 0;
 
-        private String name;
+        for (Map.Entry<String, List<String>> c : bag.entrySet()) {
+            String name = c.getKey();
+            ArrayList<String> list = new ArrayList<>();
+            list.addAll(c.getValue());
+/*
+            switch (list.size()) {
+                case 1:
+                    break;
+                case 4:
+                    if (list[1].equalsIgnoreCase(color) || list[3].equalsIgnoreCase(color)) {
+                        containsGold.add(name);
+                        String color2 = name;
+                        containsGold.addAll(searchBag(color2, bags));
+                    }
+                    break;
+                case 2:
+                    if (list[1].equalsIgnoreCase(color)) {
+                        containsGold.add(name);
+                        String color3 = name;
+                        containsGold.addAll(searchBag(color3, bags));
+                    }
+                    break;
+            }
 
+ */
 
-
-        private List<String> insideBags = new ArrayList<>();
-
-        Bags(String name) {
-            this.name = name;
+            if (!list.contains(color)) {
+                String color2 = name;
+                containsGold.add(name);
+                for (int i = 1 ; i < list.size(); i += 2) {
+                    containsGold.addAll(searchBag(color2, bag));
+                }
+            }
         }
-
-        Bags(String name, List<String> insideBags){
-            this.insideBags = insideBags;
-            this.name = name;
-            System.out.println(name + "-" + insideBags);
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public List<String> getInsideBags() {
-            return insideBags;
-        }
-
-        public void setInsideBags(List<String> insideBags) {
-            this.insideBags = insideBags;
-        }
+        System.out.println(containsGold);
+        return containsGold;
     }
 }
